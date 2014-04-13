5c58d4db2536aa67f0c3ceda9d6e06cf557e5f3e
Robin Lee <robincrlee@gmail.com>
2014-04-12 20:13:36 -0700

    Added Guides Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController # define the file
+  def home 
+  	# this has something to do with the homepage. 
+  	# its empty becuase we're not interacting with DB yet. 
+  end
+
+  def about # we have two pages
+  end
+
+  def guides
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = 'a2ec6ffba1a3ea9c4b7e5f906cdeaf83a9bb328845451e881845eb0f17f18ebbd5beeca0e842dd47ec290948b5aef23e0e1f0b2f933c63c1d0abd30b9a0e1f36'
